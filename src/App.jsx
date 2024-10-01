73835c93577927a93b771c166ef8fc88bd524110
Cleidson Ramos Fias <116103790+CFias@users.noreply.github.com>
2024-09-30 21:42:46 -0300

    feat: Created Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Banner } from "./components/Banner/Banner"
+import { Navbar } from "./components/Banner/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+     <Banner />
+    </>
+  )
+}
+
+export default App
